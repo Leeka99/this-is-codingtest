fa69d3926a5f490f096a5f8b799007c2264c26dd
Leeka99 <dsrn0000@gmail.com>
2026-02-19 11:42:11 +0900

    feat: 재귀 함수로 이진탐색 구현하기

---
+++ part2/binarysearch/binarysearchcode/recursivefunction/RecursiveFunction.java
@@ -0,0 +1,57 @@
+package part2.binarysearch.binarysearchcode.recursivefunction;
+
+import java.util.*;
+
+// 정렬된 정수들이 입력된다.
+public class RecursiveFunction {
+
+    private static int n, target, start, end;
+    private static int[] arr;
+
+    private static int binarySearch() {
+
+        if (start > end)
+            return -1;
+
+        int mid = (start + end) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        else if (arr[mid] < target) {
+            start = mid;
+            return binarySearch();
+        }
+
+        else {
+            end = mid;
+            return binarySearch();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        target = sc.nextInt();
+
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        start = 0;
+        end = n - 1;
+
+        int answer = binarySearch();
+
+        if (answer == -1) {
+            System.out.println("원소가 존재하지 않습니다.");
+        } else {
+            System.out.println(answer + 1);
+        }
+    }
+}
