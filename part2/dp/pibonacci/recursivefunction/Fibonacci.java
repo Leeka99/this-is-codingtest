72d5fef4495e6da06c71d1ec2ffd130ee9dc7dde
Leeka99 <dsrn0000@gmail.com>
2026-02-22 15:02:43 +0900

    feat: 재귀 함수만 이용해서 피보나치 수열 구현

---
+++ part2/dp/pibonacci/recursivefunction/Fibonacci.java
@@ -0,0 +1,15 @@
+package part2.dp.pibonacci.recursivefunction;
+
+public class Fibonacci {
+
+    private static int fibo(int n) {
+        if (n == 1 || n == 2)
+            return 1;
+
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibo(4));
+    }
+}
